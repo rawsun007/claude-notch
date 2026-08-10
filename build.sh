2ccd54e72f00eb7232e1ca6f694cd9d70edaeec3
rawsun007 <roshanramani.dev@gmail.com>
2026-08-10 11:53:35 +0530

    Release v0.21.0

--- build.sh
+++ build.sh
@@ -98,8 +98,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleDisplayName</key><string>ClaudeNotch</string>
     <key>CFBundleExecutable</key><string>ClaudeNotch</string>
     <key>CFBundleIconFile</key><string>AppIcon</string>
-    <key>CFBundleShortVersionString</key><string>0.20.1</string>
-    <key>CFBundleVersion</key><string>125</string>
+    <key>CFBundleShortVersionString</key><string>0.21.0</string>
+    <key>CFBundleVersion</key><string>126</string>
     <key>CFBundlePackageType</key><string>APPL</string>
     <key>LSMinimumSystemVersion</key><string>13.0</string>
     <key>LSUIElement</key><true/>
